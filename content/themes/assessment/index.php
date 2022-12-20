e36179e0354e23e6ae9e858df4904df419346c53
Scott Weaver <scottmweaver@gmail.com>
2022-12-19 17:16:22 -0800

    Fix index.php

--- content/themes/assessment/index.php
+++ content/themes/assessment/index.php
@@ -4,15 +4,15 @@
  */
 
 ?>
-<?php get_header(); ?>
-<div>
-    <h1>themes/headless/index.php</h1>
-    <p>Edit me to make your headless theme!</p>
-    <div class="info-box">
-        Log in to admin at <a href="/wp-admin/">/wp-admin/</a>
-        with username <strong>admin</strong> and password <strong>password</strong>.
+<?php
+get_header(); ?>
+    <div>
+        <h1>themes/headless/index.php</h1>
+        <p>Edit me to make your theme!</p>
+        <div class="info-box">
+            Log in to admin at <a href="<?php echo esc_url( home_url( 'wp-admin' ) ); ?>">/wp-admin/</a>
+            with username <strong>admin</strong> and password <strong>password</strong>.
+        </div>
     </div>
-    <div id="app"></div>
-</div>
 <?php
 get_footer();
