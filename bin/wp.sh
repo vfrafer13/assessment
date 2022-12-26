899d541ccb2799f7e71a3247df97a5ef9ae7adde
vfrafer13 <vfrafer13@gmail.com>
2022-12-26 00:21:30 -0600

    update wp.sh with weavers changes

--- bin/wp.sh
+++ bin/wp.sh
@@ -1,4 +1,20 @@
+
 #!/usr/bin/env bash
 
+###################################
+# wp-cli helper
+#
+# Usage: ./bin/wp.sh 'command'
+#
+# Example: Get a list of users.
+# Command: ./bin/wp.sh 'user list'
+#
+# Example: Update admin user password.
+# Command: ./bin/wp.sh 'user update admin --user_pass=admin'
+#
+# Example: Create post with 'page' post type.
+# Command: ./bin/wp.sh 'post create --post_type=page --post_title="New Page" --post_content="Hello World"'
+##################################
+
 npx wp-env run cli \
-    "${@:-help core}" 2>/dev/null
+    "wp ${@:-help}" 2>/dev/null
